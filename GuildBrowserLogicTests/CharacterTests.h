4fe1dcd01fe81ef7c8c1b943b4d53a074d4c95fc
Christopher Ungeheier <chris.ungeheier@gmail.com>
2012-10-18 14:10:28 -0700

    tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by DVUSD Programmer on 10/18/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
